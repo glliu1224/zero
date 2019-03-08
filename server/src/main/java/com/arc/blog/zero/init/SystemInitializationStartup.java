0f17aa776d33d9ff51cb456ae19659304dcd9f0f
may <lamymay@outlook.com>
2019-03-08 23:09:42 +0800

    security init 0.2

--- server/src/main/java/com/arc/blog/zero/init/SystemInitializationStartup.java
+++ server/src/main/java/com/arc/blog/zero/init/SystemInitializationStartup.java
@@ -1,6 +1,7 @@
 package com.arc.blog.zero.init;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -15,16 +16,21 @@ import org.springframework.stereotype.Component;
 public class SystemInitializationStartup implements ApplicationListener<ContextRefreshedEvent> {
 
 
-    //建议配置在配置文件中
-    //	@Value("${system.initial:false}")
-    boolean initial = Boolean.TRUE;
+    /**
+     * web.system.initial:是否在系统启动的时候初始化一些操作
+     * 注意：1、建议配置在配置文件中 2、缺省为true
+     */
+    	@Value("${web.system.initial:true}")
+    boolean initial ;
 
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         //Spring容器加载完毕之后执行: 以下方法
         if (initial) {
-            System.out.println("#####Spring容器加载完毕之后执行");
+            System.out.println("#########################");
+            System.out.println(" Spring容器加载完毕之后执行的方法可以做一些扩展  ");
+            System.out.println("#########################");
         }
     }
 
