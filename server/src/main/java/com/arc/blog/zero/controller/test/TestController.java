cb10f93551fd05f828e3907c8ea903b7f061fbfc
lamymay <lamymay@outlook.com>
2019-03-14 12:44:17 +0800

    深克隆方法2，之序列话饭序列化测试

---
+++ server/src/main/java/com/arc/blog/zero/controller/test/TestController.java
@@ -0,0 +1,16 @@
+package com.arc.blog.zero.controller.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 叶超
+ * @since 2019/3/14 11:21
+ */
+@Slf4j
+@RestController
+public class TestController {
+
+
+
+}
